8a1113869782e6988718b1a45f71cc0a58e8207e
makcgr <max.grachev@gmail.com>
2015-12-29 17:25:39 +0300

    Added new templates and features, refactored binding
    new Templates: RTSMCallEvent for chameleon.log,
    ServerInterface.UpdateCall for otcpc.log
    
    new features: add new line after each parsed message

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,9 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.rtResults = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.txtMsgRegex = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -43,10 +44,24 @@
             this.label3 = new System.Windows.Forms.Label();
             this.tcSpecific = new System.Windows.Forms.TabControl();
             this.tpTHS = new System.Windows.Forms.TabPage();
-            this.label4 = new System.Windows.Forms.Label();
             this.cbIncludePresenceMsgs = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbAddLine = new System.Windows.Forms.CheckBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dsTemplates = new System.Data.DataSet();
+            this.tTemplates = new System.Data.DataTable();
+            this.dataColumn1 = new System.Data.DataColumn();
+            this.dataColumn2 = new System.Data.DataColumn();
+            this.dataColumn3 = new System.Data.DataColumn();
+            this.dataColumn4 = new System.Data.DataColumn();
+            this.dataColumn5 = new System.Data.DataColumn();
+            this.dataColumn6 = new System.Data.DataColumn();
+            this.dataColumn7 = new System.Data.DataColumn();
             this.tcSpecific.SuspendLayout();
             this.tpTHS.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsTemplates)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tTemplates)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -71,16 +86,16 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // richTextBox2
+            // rtResults
             // 
-            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.rtResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox2.Location = new System.Drawing.Point(12, 422);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(815, 161);
-            this.richTextBox2.TabIndex = 0;
-            this.richTextBox2.Text = "Result";
+            this.rtResults.Location = new System.Drawing.Point(12, 422);
+            this.rtResults.Name = "rtResults";
+            this.rtResults.Size = new System.Drawing.Size(815, 161);
+            this.rtResults.TabIndex = 0;
+            this.rtResults.Text = "Result";
             // 
             // button2
             // 
@@ -98,6 +113,7 @@
             // 
             this.txtMsgRegex.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMsgRegex.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bindingSource1, "Regex", true));
             this.txtMsgRegex.Location = new System.Drawing.Point(115, 180);
             this.txtMsgRegex.Multiline = true;
             this.txtMsgRegex.Name = "txtMsgRegex";
@@ -117,11 +133,11 @@
             // 
             this.txtDateRegex.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDateRegex.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bindingSource1, "DateTimeRegex", true));
             this.txtDateRegex.Location = new System.Drawing.Point(115, 222);
             this.txtDateRegex.Name = "txtDateRegex";
             this.txtDateRegex.Size = new System.Drawing.Size(297, 20);
             this.txtDateRegex.TabIndex = 3;
-            this.txtDateRegex.Text = "(?<Year>(?:\\d{4}))-(?<Month>\\d{2})-(?<Day>\\d{2})\\s\\d{2}:\\d{2}:\\d{2},\\d{6}\\s";
             // 
             // label2
             // 
@@ -145,6 +161,9 @@
             // cmbMsgRegex
             // 
             this.cmbMsgRegex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbMsgRegex.DataBindings.Add(new System.Windows.Forms.Binding("SelectedItem", this.bindingSource1, "DisplayName", true));
+            this.cmbMsgRegex.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.bindingSource1, "Id", true));
+            this.cmbMsgRegex.DataSource = this.bindingSource1;
             this.cmbMsgRegex.FormattingEnabled = true;
             this.cmbMsgRegex.Location = new System.Drawing.Point(473, 179);
             this.cmbMsgRegex.Name = "cmbMsgRegex";
@@ -203,16 +222,6 @@
             this.tpTHS.Text = "THS log";
             this.tpTHS.UseVisualStyleBackColor = true;
             // 
-            // label4
-            // 
-            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(425, 240);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Specific";
-            // 
             // cbIncludePresenceMsgs
             // 
             this.cbIncludePresenceMsgs.AutoSize = true;
@@ -223,11 +232,85 @@
             this.cbIncludePresenceMsgs.Text = "Include presence messages";
             this.cbIncludePresenceMsgs.UseVisualStyleBackColor = true;
             // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(425, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Specific";
+            // 
+            // cbAddLine
+            // 
+            this.cbAddLine.AutoSize = true;
+            this.cbAddLine.Location = new System.Drawing.Point(12, 313);
+            this.cbAddLine.Name = "cbAddLine";
+            this.cbAddLine.Size = new System.Drawing.Size(199, 17);
+            this.cbAddLine.TabIndex = 11;
+            this.cbAddLine.Text = "Add additional line for each message";
+            this.cbAddLine.UseVisualStyleBackColor = true;
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataMember = "Templates";
+            this.bindingSource1.DataSource = this.dsTemplates;
+            // 
+            // dsTemplates
+            // 
+            this.dsTemplates.DataSetName = "TemplatesDS";
+            this.dsTemplates.Tables.AddRange(new System.Data.DataTable[] {
+            this.tTemplates});
+            // 
+            // tTemplates
+            // 
+            this.tTemplates.Columns.AddRange(new System.Data.DataColumn[] {
+            this.dataColumn1,
+            this.dataColumn2,
+            this.dataColumn3,
+            this.dataColumn4,
+            this.dataColumn5,
+            this.dataColumn6,
+            this.dataColumn7});
+            this.tTemplates.TableName = "Templates";
+            // 
+            // dataColumn1
+            // 
+            this.dataColumn1.ColumnName = "Id";
+            this.dataColumn1.DataType = typeof(int);
+            // 
+            // dataColumn2
+            // 
+            this.dataColumn2.ColumnName = "Regex";
+            // 
+            // dataColumn3
+            // 
+            this.dataColumn3.ColumnName = "DisplayName";
+            // 
+            // dataColumn4
+            // 
+            this.dataColumn4.ColumnName = "DirectionInRegex";
+            // 
+            // dataColumn5
+            // 
+            this.dataColumn5.ColumnName = "DirectionOutRegex";
+            // 
+            // dataColumn6
+            // 
+            this.dataColumn6.ColumnName = "AddLineAfter";
+            this.dataColumn6.DataType = typeof(bool);
+            // 
+            // dataColumn7
+            // 
+            this.dataColumn7.ColumnName = "DateTimeRegex";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(839, 619);
+            this.Controls.Add(this.cbAddLine);
             this.Controls.Add(this.tcSpecific);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -241,7 +324,7 @@
             this.Controls.Add(this.txtMsgRegex);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.rtResults);
             this.Controls.Add(this.richTextBox1);
             this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "Form1";
@@ -249,6 +332,9 @@
             this.tcSpecific.ResumeLayout(false);
             this.tpTHS.ResumeLayout(false);
             this.tpTHS.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsTemplates)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tTemplates)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -258,7 +344,7 @@
 
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox rtResults;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox txtMsgRegex;
         private System.Windows.Forms.Label label1;
@@ -273,6 +359,17 @@
         private System.Windows.Forms.TabPage tpTHS;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.CheckBox cbIncludePresenceMsgs;
+        private System.Windows.Forms.CheckBox cbAddLine;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Data.DataSet dsTemplates;
+        private System.Data.DataTable tTemplates;
+        private System.Data.DataColumn dataColumn1;
+        private System.Data.DataColumn dataColumn2;
+        private System.Data.DataColumn dataColumn3;
+        private System.Data.DataColumn dataColumn4;
+        private System.Data.DataColumn dataColumn5;
+        private System.Data.DataColumn dataColumn6;
+        private System.Data.DataColumn dataColumn7;
     }
 }
 
